1a02eca743ba378c5bef8c40adf3291916cc7e49
Shruti Kesarwani <shrutikesarwani@Mac.lan>
2026-06-22 13:30:49 +0530

    Added Week 4 Day 1 solutions

---
+++ Week_4/Day_1/Max_depth_tree.java
@@ -0,0 +1,47 @@
+package Week_4.Day_1;
+
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Max_depth_tree {
+
+    public static int maxDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+    public static void main(String[] args) {
+
+        /*
+                 1
+                / \
+               2   3
+              /
+             4
+        */
+
+        TreeNode root = new TreeNode(1);
+
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        root.left.left = new TreeNode(4);
+
+        System.out.println("Maximum Depth = " + maxDepth(root));
+    }
+}
